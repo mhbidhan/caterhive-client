82fbbd030f36e8fc4bad580088c8ee648bc3a2ad
Mahamud <mhbidhan@outlook.com>
2023-04-16 12:52:27 +0600

    Modified Authenticated component to add routing to the app and added all the nedded components to it

--- src/router/Authenticated/Authenticated.component.jsx
+++ src/router/Authenticated/Authenticated.component.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from '../../pages/Home/Home.page';
+import SplitScreen from './../../components/SplitScreen/SplitScreen.component';
+import MenuPreview from './../../pages/MenuPreview/MenuPreview.component';
+import MyProfile from './../../pages/MyProfile/MyProfile.page';
 
 const Authenticated = () => {
-  return <div>Authenticated</div>;
+  const routes = [
+    {
+      path: '/',
+      label: 'home',
+      element: <Home />,
+      sidebar: true,
+    },
+    {
+      path: '/profile',
+      label: 'My Profile',
+      element: <MyProfile />,
+      sidebar: true,
+    },
+    {
+      path: '/menu/:menuId',
+      label: 'Menu',
+      element: <MenuPreview />,
+    },
+  ];
+  return (
+    <div>
+      <SplitScreen routes={routes}>
+        <Routes>
+          {routes.map((route) => {
+            const { path, element } = route;
+            return <Route key={path} path={path} element={element} />;
+          })}{' '}
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </SplitScreen>
+    </div>
+  );
 };
 
 export default Authenticated;
