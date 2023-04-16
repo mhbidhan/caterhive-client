1f22256e0cfaf21dc2c8523dcffe1395ab9f43b9
Mahamud <mhbidhan@outlook.com>
2023-04-16 12:50:57 +0600

    Defined empty Home Page and styles for it

---
+++ src/pages/Home/Home.page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SidebarButton from './../../components/SidebarButton/SidebarButton.component';
+
+const Home = () => {
+  return (
+    <div className="home container">
+      <SidebarButton />
+    </div>
+  );
+};
+
+export default Home;
