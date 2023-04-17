61ad8eb518af620b0a0c26d58a8797b8cb487a9b
Mahamud <mhbidhan@outlook.com>
2023-04-17 18:07:48 +0600

    Modified CatererCard to implement bookmarking

--- src/components/CatererCard/CatererCard.component.jsx
+++ src/components/CatererCard/CatererCard.component.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Tag from '../common/Tag/Tag.component';
+import useBookmark from './../../hooks/useBookmark';
 import Badge from './../common/Badge/Badge.component';
 import BookmarkButton from './../common/BookmarkButton/BookmarkButton.component';
 import './CatererCard.styles.scss';
 
-const CatererCard = ({ caterer, bookmarked = false, handleClick }) => {
-  const handleBookmark = (e) => {
-    e.stopPropagation();
-    // TODO: Implement bookmark.
-  };
+const CatererCard = ({ caterer, handleClick }) => {
+  const { bookmarkCaterer } = useBookmark();
+  const user = useSelector((state) => state.user);
 
   if (!caterer) return;
 
-  const { businessName, brandImg, activeDays, operationalAreas, reviews } =
+  const { _id, businessName, brandImg, activeDays, operationalAreas, reviews } =
     caterer;
   const rating =
     reviews.reduce((acc, curr) => acc + curr.rating, 0) / reviews.length;
 
+  const handleBookmark = (e) => {
+    e.stopPropagation();
+    bookmarkCaterer(_id);
+  };
+
   return (
     <div className="caterer-card" onClick={handleClick}>
       <span className="bookmark">
-        <BookmarkButton handleClick={handleBookmark} bookmarked={bookmarked} />
+        <BookmarkButton
+          handleClick={handleBookmark}
+          bookmarked={user.bookmarks.caterers.includes(_id)}
+        />
       </span>
       <div className="brand-img-container">
         <img src={brandImg} alt="" className="brand-img" />
