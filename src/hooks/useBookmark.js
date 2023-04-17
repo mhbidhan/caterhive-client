ac66d3b351c1a833cd7432aeef412861d3cda3c9
Mahamud <mhbidhan@outlook.com>
2023-04-17 18:14:55 +0600

    Modified useBookmark hook to add caterer bookmarking function

--- src/hooks/useBookmark.js
+++ src/hooks/useBookmark.js
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { setUser } from '../redux/redducers/user';
+import { bookmarkCatererById } from '../services/caterer';
 import { bookmarkMenuById } from '../services/menu';
 
 const useBookmark = () => {
@@ -10,8 +11,14 @@ const useBookmark = () => {
     dispatch(setUser(user));
   };
 
+  const bookmarkCaterer = async (id) => {
+    const user = await bookmarkCatererById(id);
+    dispatch(setUser(user));
+  };
+
   return {
     bookmarkMenu,
+    bookmarkCaterer,
   };
 };
 
