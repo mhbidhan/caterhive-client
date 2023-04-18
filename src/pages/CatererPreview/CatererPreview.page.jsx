8e87adae2e3a7f09f0f11bf0dd163ee7ccf8509f
Your Name <you@example.com>
2023-04-18 11:32:15 +0600

    Defined CatererPreview page and styles for it

---
+++ src/pages/CatererPreview/CatererPreview.page.jsx
@@ -0,0 +1,53 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import DayMenuCard from '../../components/DayMenuCard/DayMenuCard.component';
+import PageDecorator from '../../components/PageDecorator/PageDecorator.component';
+import SidebarButton from '../../components/SidebarButton/SidebarButton.component';
+import { setLoading } from '../../redux/redducers/loading';
+import { getCatererById } from '../../services/caterer';
+import './CatererPreview.styles.scss';
+
+const CatererPreview = () => {
+  const [caterer, setCaterer] = useState(null);
+  const { catererId } = useParams();
+  const dispatch = useDispatch();
+
+  const getCaterer = useCallback(async () => {
+    dispatch(setLoading(true));
+    const caterer = await getCatererById(catererId);
+
+    setCaterer(caterer);
+    dispatch(setLoading(false));
+  }, [setCaterer, catererId, dispatch]);
+
+  useEffect(() => {
+    getCaterer();
+  }, [getCaterer]);
+
+  if (!caterer) return null;
+
+  const { businessName, email, brandImg, weekMenu } = caterer;
+
+  return (
+    <PageDecorator circle={false}>
+      <div className="container caterer-preview">
+        <SidebarButton />
+        <div className="caterer-intro">
+          <img src={brandImg} alt="" className="caterer-brand-logo avatar" />
+          <h3>{businessName}</h3>
+          <p>{email}</p>
+        </div>
+        <div className="day-menu-container">
+          {weekMenu
+            ? Object.keys(weekMenu).map((day) => (
+                <DayMenuCard dayMenu={weekMenu[day]} day={day} />
+              ))
+            : null}
+        </div>
+      </div>
+    </PageDecorator>
+  );
+};
+
+export default CatererPreview;
