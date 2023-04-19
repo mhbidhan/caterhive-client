81543aef7473e8b1237b067b1728730bc3c99ad2
Mahamud <mhbidhan@outlook.com>
2023-04-19 21:14:55 +0600

    Defined a validator for NewOrderForm added payment processing functionalities to it

--- src/components/NewOrderForm/NewOrderForm.component.jsx
+++ src/components/NewOrderForm/NewOrderForm.component.jsx
@@ -1,14 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { NewOrderContext } from '../../pages/NewOrder/NewOrder.page';
+import { makePayment } from '../../services/payment';
 import OrderCart from '../NewOrderCart/NewOrderCart.component';
 import CustomButton from './../common/CustomButton/CustomButton.component';
 import InputField from './../common/InputField/InputField.component';
 import './NewOrderForm.styles.scss';
+import newOrderValidatorSchema from './newOrderFrom.validator';
 
 const NewOrderForm = () => {
-  const { formData, setFormData, setView } = useContext(NewOrderContext);
+  const { formData, setFormData, setView, amount } =
+    useContext(NewOrderContext);
+  const user = useSelector((state) => state.user);
+  const [errorMessages, setErrorMessages] = useState(null);
 
   const handleChange = (e) => {
+    setErrorMessages(null);
     const { name, value } = e.target;
 
     setFormData({
@@ -19,15 +26,43 @@ const NewOrderForm = () => {
 
   const { shippingAddress, specialInstruction } = formData;
 
+  const handlePayment = async (e) => {
+    e.preventDefault();
+
+    const { error } = newOrderValidatorSchema.validate(formData);
+
+    console.log(error);
+    if (error) {
+      setErrorMessages({
+        [error?.details[0]?.path[0]]: error?.message,
+      });
+      return;
+    }
+
+    localStorage.setItem(
+      'orderCreds',
+      JSON.stringify({ ...formData, orderValue: amount })
+    );
+
+    makePayment({
+      amount,
+      shippingAddress,
+      customerEamil: user.email,
+      customerPhone: user.phone,
+      customerName: user.fullName,
+    });
+  };
+
   return (
     <div>
-      <form onSubmit={() => setView(<>Payment</>)} className="new-order-from">
+      <form onSubmit={handlePayment} className="new-order-from">
         <InputField
           label={'Shipping Address'}
           name={'shippingAddress'}
           value={shippingAddress}
           element="textArea"
           handleChange={handleChange}
+          error={errorMessages?.shippingAddress}
         />
         <InputField
           label={'Special Instruction'}
@@ -35,13 +70,18 @@ const NewOrderForm = () => {
           value={specialInstruction}
           element="textArea"
           handleChange={handleChange}
+          error={errorMessages?.specialInstruction}
         />
         <div className="btn-container">
           <CustomButton
             label={'Back'}
             handleClick={() => setView(<OrderCart />)}
           />
-          <CustomButton label={'Payment'} primary={true} />
+          <CustomButton
+            handleClick={handlePayment}
+            label={'Payment'}
+            primary={true}
+          />
         </div>
       </form>
     </div>
---
+++ src/components/NewOrderForm/newOrderFrom.validator.js
@@ -0,0 +1,15 @@
+import joi from 'joi';
+
+const newOrderValidatorSchema = joi.object({
+  caterer: joi.string().min(3).max(255).required().label('Caterer'),
+  orderedProducts: joi.array().label('orderedProducts'),
+  shippingAddress: joi
+    .string()
+    .min(3)
+    .max(255)
+    .required()
+    .label('Shipping Address'),
+  specialInstruction: joi.string().min(3).max(255).label('Special Instruction'),
+});
+
+export default newOrderValidatorSchema;
