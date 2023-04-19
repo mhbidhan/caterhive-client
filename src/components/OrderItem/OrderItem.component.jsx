9c8f66c6d400810b663ed649ec83aacf86823b59
Mahamud <mhbidhan@outlook.com>
2023-04-19 12:05:17 +0600

    Defined OrderItem component and styles for it

---
+++ src/components/OrderItem/OrderItem.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './OrderItem.styles.scss';
+
+const OrderItem = ({ order }) => {
+  const navigate = useNavigate();
+  if (!order) return;
+  const { _id, caterer, orderStatus, orderValue } = order;
+  return (
+    <div onClick={() => navigate(`/myOrders/${_id}`)} className="order-item">
+      <div className="content">
+        <div className="caterer-details">
+          <img src={caterer.brandImg} className="caterer-logo" alt="" />
+          <div>
+            <p className="caterer-name">{caterer.businessName}</p>
+          </div>
+        </div>
+        <div
+          className={`status ${orderStatus === 'delivered' ? 'delivered' : ''}`}
+        >
+          {orderStatus}
+        </div>
+      </div>
+      <p className="value">order value: {orderValue}</p>
+    </div>
+  );
+};
+
+export default OrderItem;
