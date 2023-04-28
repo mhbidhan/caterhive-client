5d62e90df932169b5d5adbe4d04e76c6417d8a22
Mahamud <mhbidhan@outlook.com>
2023-04-28 20:00:26 +0600

    Modified the OrderItem component to add a api props that decides which url to hit

--- src/components/OrderItem/OrderItem.component.jsx
+++ src/components/OrderItem/OrderItem.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './OrderItem.styles.scss';
 
-const OrderItem = ({ order }) => {
+const OrderItem = ({ order, api = 'myOrders' }) => {
   const navigate = useNavigate();
   if (!order) return;
   const { _id, caterer, orderStatus, orderValue } = order;
   return (
-    <div onClick={() => navigate(`/myOrders/${_id}`)} className="order-item">
+    <div onClick={() => navigate(`/${api}/${_id}`)} className="order-item">
       <div className="content">
         <div className="caterer-details">
           <img src={caterer.brandImg} className="caterer-logo" alt="" />
