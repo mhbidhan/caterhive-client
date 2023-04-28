9183fd8e78bf65423608bc8e3661b36231ca734a
Mahamud <mhbidhan@outlook.com>
2023-04-28 20:04:07 +0600

    Modified MyOrders page
    - Added a tab that provides option to view both the event service orders and regular orders

--- src/pages/MyOrders/MyOrders.page.jsx
+++ src/pages/MyOrders/MyOrders.page.jsx
@@ -1,11 +1,29 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import useLoading from '../../hooks/useLoading';
+import { getOwnEventServiceOrders } from '../../services/eventServiceOrder';
 import { getOwnOrder } from '../../services/order';
 import OrderItem from './../../components/OrderItem/OrderItem.component';
 import SidebarButton from './../../components/SidebarButton/SidebarButton.component';
+import Tab from './../../components/common/Tab/Tab.component';
 import './MyOrders.styles.scss';
 
 const MyOrders = () => {
+  const tabData = [
+    { id: 'orrders', label: 'Regular Orders', content: <RegularOrderList /> },
+    { id: 'eventOrders', label: 'Event Orders', content: <EventOrderList /> },
+  ];
+  return (
+    <div className="container my-order-page">
+      <SidebarButton />
+      <h1 className="page-title">My Orders</h1>
+      <Tab id={'orderList'} tabData={tabData} />
+    </div>
+  );
+};
+
+export default MyOrders;
+
+const RegularOrderList = () => {
   const [orders, setOrders] = useState([]);
   const { startLoading, stopLoading } = useLoading();
 
@@ -27,11 +45,9 @@ const MyOrders = () => {
   }, [getOrders]);
 
   return (
-    <div className="container my-order-page">
-      <SidebarButton />
-      <h1 className="page-title">My Order</h1>
+    <div>
       {orders.length ? (
-        orders.map((oo) => <OrderItem key={oo._id} order={oo} />)
+        orders.map((order) => <OrderItem key={order._id} order={order} />)
       ) : (
         <div>No Orders Yet</div>
       )}
@@ -39,4 +55,36 @@ const MyOrders = () => {
   );
 };
 
-export default MyOrders;
+const EventOrderList = () => {
+  const [orders, setOrders] = useState([]);
+  const { startLoading, stopLoading } = useLoading();
+
+  const getOrders = useCallback(async () => {
+    try {
+      startLoading();
+      const orders = await getOwnEventServiceOrders();
+
+      setOrders(orders);
+      stopLoading();
+    } catch (error) {
+      stopLoading();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    getOrders();
+  }, [getOrders]);
+
+  return (
+    <div>
+      {orders.length ? (
+        orders.map((order) => (
+          <OrderItem key={order._id} order={order} api="eventOrder" />
+        ))
+      ) : (
+        <div>No Orders Yet</div>
+      )}
+    </div>
+  );
+};
